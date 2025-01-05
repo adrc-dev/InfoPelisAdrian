223a1032da8d901978b7df41ea6ab6313ba830ac
adr <adr.crespo93@gmail.com>
2025-01-05 21:54:57 +0100

    algunos cambios en los estilos de los logos

--- resources/views/partials/headers.blade.php
+++ resources/views/partials/headers.blade.php
@@ -1,7 +1,14 @@
 <header>
-    <a href="{{route('index')}}"><img id="indexLogo" class="logo" src="{{asset('images/infopelis.jpeg')}}" alt="logo infopelis"></a>
-    <a href="{{route('movies.index')}}"><img id="moviesLogo" class="logo" src="{{asset('images/peliculas.png')}}" alt="logo entradas"></a>
-    <a href="{{route('movieDirectors.index')}}"><img class="logo" src="{{asset('images/director.png')}}" alt="logo directores"></a>
-    <a href="{{route('actors')}}"><img class="logo" src="{{asset('images/actor.png')}}" alt="logo actores"></a>
-    <a href="{{route('movies.create')}}"><img class="logo" src="{{asset('images/agregar.png')}}" alt="logo agregar pelicula"></a>
+    <a href="{{ route('index') }}"><img id="indexLogo" src="{{ asset('images/infopelis.jpeg') }}"
+            alt="logo infopelis"></a>
+    <div class="header-logos">
+        <a href="{{ route('movies.index') }}"><img id="moviesLogo" class="logo" src="{{ asset('images/peliculas.png') }}"
+                alt="logo entradas"></a>
+        <a href="{{ route('movieDirectors.index') }}"><img class="logo" src="{{ asset('images/director.png') }}"
+                alt="logo directores"></a>
+        <a href="{{ route('actors') }}"><img class="logo" src="{{ asset('images/actor.png') }}"
+                alt="logo actores"></a>
+        <a href="{{ route('movies.create') }}"><img class="logo" src="{{ asset('images/agregar.png') }}"
+                alt="logo agregar pelicula"></a>
+    </div>
 </header>
